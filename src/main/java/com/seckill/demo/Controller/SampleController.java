bb74185daab56e8181bff81c11a08423a5f02102
sthgreat <284387885@qq.com>
2019-11-05 21:06:19 +0800

    集成thymeleaf

--- src/main/java/com/seckill/demo/Controller/SampleController.java
+++ src/main/java/com/seckill/demo/Controller/SampleController.java
@@ -5,6 +5,7 @@ import com.seckill.demo.Service.UserService;
 import com.seckill.demo.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -25,5 +26,11 @@ public class SampleController {
     @ResponseBody
     public Result<String> hello(){
         return Result.success("hello!");
+    }
+
+    @RequestMapping("/thymeleaf")
+    public String thymeleaf(Model model){
+        model.addAttribute("name","jsb");
+        return "hello";
     }
 }
--- src/main/java/com/seckill/demo/Dao/UserMapper.java
+++ src/main/java/com/seckill/demo/Dao/UserMapper.java
@@ -1,6 +1,7 @@
 package com.seckill.demo.Dao;
 
 import com.seckill.demo.domain.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -9,6 +10,10 @@ import org.springframework.stereotype.Component;
 @Mapper
 @Component(value = "UserMapper")
 public interface UserMapper {
-    @Select("select * from user where id = #{UserId}")
+
+    @Select("select * from user where user_id = #{UserId}")
     public User getById(@Param(value = "UserId") Integer id);
+
+    @Insert("insert into user(user_name) values(#{UserName})")
+    void addUser(User user);
 }
