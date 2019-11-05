5296fd482b67674f39c80eae7cf0fea8713cd552
sthgreat <284387885@qq.com>
2019-11-05 21:43:07 +0800

    11.5晚

---
+++ src/main/java/com/seckill/demo/redis/RedisConfig.java
@@ -0,0 +1,10 @@
+package com.seckill.demo.redis;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "redis")
+public class RedisConfig {
+
+}
