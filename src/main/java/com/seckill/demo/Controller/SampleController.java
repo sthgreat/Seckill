df8aa360f10377883781e65f44e131df1dc2f4e7
sthgreat <284387885@qq.com>
2019-11-06 20:16:43 +0800

    本机可以跑通redis，阿里云服务器不行

--- src/main/java/com/seckill/demo/Controller/SampleController.java
+++ src/main/java/com/seckill/demo/Controller/SampleController.java
@@ -1,9 +1,13 @@
 package com.seckill.demo.Controller;
 
+import com.seckill.demo.Result.CodeMsg;
 import com.seckill.demo.Result.Result;
 import com.seckill.demo.Service.UserService;
 import com.seckill.demo.domain.User;
+import org.apache.ibatis.annotations.Arg;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +18,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class SampleController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @RequestMapping("/db/get")
     @ResponseBody
@@ -32,5 +42,17 @@ public class SampleController {
     public String thymeleaf(Model model){
         model.addAttribute("name","jsb");
         return "hello";
+    }
+
+    @RequestMapping("/redis/set")
+    @ResponseBody
+    public void set(){
+        stringRedisTemplate.opsForValue().set("1234","kkl");
+    }
+
+    @RequestMapping("/redis/get")
+    @ResponseBody
+    public String get(){
+        return stringRedisTemplate.opsForValue().get("1234");
     }
 }
--- src/main/java/com/seckill/demo/redis/RedisConfig.java
+++ src/main/java/com/seckill/demo/redis/RedisConfig.java
@@ -1,10 +1,19 @@
 package com.seckill.demo.redis;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import redis.clients.jedis.JedisPoolConfig;
 
 @Component
 @ConfigurationProperties(prefix = "redis")
+@Data
 public class RedisConfig {
-
+    private String host;
+    private int port;
+    private int timeout;
+    private String password;
+    private int poolMaxTotal;
+    private int poolMaxIdle;
+    private int poolMaxWait;
 }
